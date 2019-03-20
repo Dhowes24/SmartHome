9d526cd2ac8807d7b6912ed0cd58bc59a7c01d10
brienpacholec <brienpacholec@gmail.com>
2019-03-20 18:34:52 -0400

    finished light class, having issues with test

--- src/main/java/Light.java
+++ src/main/java/Light.java
@@ -21,31 +21,70 @@ public class Light {
 
     }
 
+    /**
+     * @return String - the ID of the light
+     */
     public String getID(){return this.ID;}
+
+    /**
+     * @return double - the current amount the light is operating on
+     */
     public double getAmount(){return this.amount;}
+
+    /**
+     * @return boolean - the current user preferences for the light
+     */
     public boolean getPreference(){return this.isPinRequired;}
+
+    /**
+     * @return double - the current preferred amount of the light
+     */
     public double getPreferredAmount(){return this.preferredAmount;}
+
+    /**
+     * @return boolean - whether the light is dimable or not
+     */
     public boolean getDimable(){return this.dimable;}
 
+    /**
+     * Sets a new value to the preferred amount
+     * @param newAmount - the new amount of the preferred amount
+     */
+    public void setPrefferedAmount(int newAmount){
+        if(amountIsValid(newAmount)){
+            this.preferredAmount = newAmount;
+        }
+    }
 
+    /**
+     * Turns the lights amount to the current preferred amount
+     */
     public void turnOn(){
         this.amount=this.preferredAmount;
     }
 
+    /**
+     * Turns the lights amount to zero turning it off
+     */
     public void turnOff(){
         this.amount=0.0;
     }
 
+    /**
+     * Dims the light up or down to the new amount
+     * @param newAmount - the new amount that the light should be at
+     */
     public void dim(double newAmount){
-        this.amount = newAmount;
-    }
-
-    public void changePreference(double newPref){
-        if(amountIsValid(newPref)){
-            this.preferredAmount=newPref;
+        if(amountIsValid(newAmount)){
+            this.amount = newAmount;
         }
     }
 
+    /**
+     * Checks to see if an amount passed in by the user is valid (Between 0 and 1 inclusive)
+     * @param amount double - the amount being checked
+     * @return boolean - whether it is a valid amount or not
+     */
     public static boolean amountIsValid(double amount){
         if(amount>=0.0 && amount <=1.0){
             return true;
@@ -55,12 +94,14 @@ public class Light {
         }
     }
 
-    public void changeIsPinRequired(){
-        if(this.isPinRequired == true){
-            this.isPinRequired=false;
-        }
-        if(this.isPinRequired == false){
-            this.isPinRequired=true;
+    /**
+     * Changes the current pin preferences for the light
+     */
+    public void changeIsPinRequired() {
+        if (this.isPinRequired == true) {
+            this.isPinRequired = false;
+        } else {
+            this.isPinRequired = true;
         }
     }
 }
--- src/test/java/LightTest.java
+++ src/test/java/LightTest.java
@@ -1,7 +1,94 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LightTest {
 
+    public Light l = new Light("lamp", true,0.75,true);
+
     @Test
-    void get
+    public void LightTest(){
+
+        Assert.assertNotNull(l);
+    }
+
+    @Test
+    public void getIDTest(){
+
+        Assert.assertEquals(l.getID(),"lamp");
+    }
+
+    @Test
+    public void getPinTest(){
+        Assert.assertEquals(l.getPreference(),true);
+    }
+
+    @Test
+    public void getDimableTest(){
+        Assert.assertEquals(l.getDimable(),true);
+    }
+
+
+    @Test
+    public void changePreferenceTest(){
+        //Change
+        l.changeIsPinRequired();
+        Assert.assertEquals(l.getPreference(),false);
+
+        //Change back
+        l.changeIsPinRequired();
+        Assert.assertEquals(l.getPreference(),true);
+    }
+
+    //TODO
+//    @Test
+//    public void isAmountValidTest(){
+//
+//    }
+
+    //TODO
+//    @Test
+//    public void dimTest(){
+//        l.dim(.96);
+//        Assert.assertEquals(l.getAmount(),.96);
+//    }
+
+
+//    //TODO
+//    @Test
+//    public void turnLightsOffTest(){
+//        l.turnOff();
+//        Assert.assertEquals(l.getAmount(),0.0);
+//
+//    }
+
+    //TODO
+//    @Test
+//    public void turnLightsOnTest(){
+//        l.turnOn();
+//        Assert.assertEquals(l.getAmount(),.75);
+//
+//    }
+
+    //TODO
+//    @Test
+//    public void getAmount(){
+//        Assert.assertEquals(l.getAmount(),.75);
+//    }
+
+    //TODO
+//    @Test
+//    public void setPreferenceTest(){
+//        //INVALID AMOUNT
+//        l.setPrefferedAmount(10);
+//        Assert.assertEquals(l.getAmount(),.75);
+//    }
+
+    //TODO
+//    @Test
+//    public void getPreferreedAmountTest(){
+//        Assert.assertEquals(l.getPreferredAmount(),0.75);
+//    }
+
+
+
 }
