91d78eba00f59796691f5cc79ade328df1313d49
Derek Howes <D.howes24@gmail.com>
2019-03-19 22:44:16 -0400

    Added functions with test shells

---
+++ src/test/java/RoomTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class RoomTest {
+
+    @Test
+    public void addApplianceTest(){
+        Room room = new Room("testRoom");
+
+    }
+
+    @Test
+    public void removeApplianceTest(){
+        Room room = new Room("testRoom");
+
+    }
+
+    @Test
+    public void changeApplianceStatusTest(){
+        Room room = new Room("testRoom");
+
+    }
+
+    @Test
+    public void addLightTest(){
+        Room room = new Room("testRoom");
+
+    }
+
+    @Test
+    public void adjustLightTest(){
+        Room room = new Room("testRoom");
+
+    }
+
+    @Test
+    public void adjustTempTest(){
+        Room room = new Room("testRoom");
+
+    }
+
+    @Test
+    public void addToScheduleTest(){
+        Room room = new Room("testRoom");
+
+    }
+
+
+}
