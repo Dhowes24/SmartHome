9304522ee1d5194c3696cbfe1aa7a59e308630de
Derek Howes <D.howes24@gmail.com>
2019-03-21 10:35:25 -0400

    Room lists and tests

---
+++ src/test/java/HouseTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+public class HouseTest {
+
+    @Test
+    public void addRoomTest(){
+
+    }
+
+    @Test
+    public void removeRoomTest(){
+
+    }
+
+    @Test
+    public void printScheduleListTest(){
+
+    }
+
+    @Test
+    public void changeHouseTempTest(){
+
+    }
+
+    @Test
+    public void turnApplicancesOffTest(){
+
+    }
+
+    @Test
+    public void turnOffLightsTest(){
+
+    }
+}
