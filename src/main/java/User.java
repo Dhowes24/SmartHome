b63ca7a6378149374e9106a7e8265da0ff5b6935
Derek Howes <D.howes24@gmail.com>
2019-03-28 19:07:24 -0400

    User and userTest Classes along with a couple functions to the House class to accesss info

---
+++ src/main/java/User.java
@@ -0,0 +1,81 @@
+import java.util.HashMap;
+
+public class User {
+    House House;
+    String Name;
+    //Preferences preferences;
+    //Permissions permissions;
+
+    public User(House house, String Name){
+        //Preferences preferences = new Preferences();
+        //Permissions permissions = new Permissions();
+        this.House = house;
+        this.Name = Name;
+
+    }
+
+    /**
+     * Creates a user and adds them to the house user list
+     * Can set preferences permissions in user creation
+     * @param Name
+     * @return
+     */
+    public String createUser(String Name){
+        if(!this.House.userList.containsKey(Name)) {
+            User newUser = new User(this.House, Name);
+            this.House.addUser(newUser);
+            return ("User successfully created");
+        }
+        else{
+            return ("User could not be created because name is already in use");
+        }
+
+    }
+
+    /**
+     * deletes a user from the house user list
+     * @param Name
+     * @return
+     */
+    public String deleteUser(String Name){
+        if(this.House.userList.containsKey(Name)){
+            this.House.userList.remove(Name);
+            return ("User successfully deleted");
+        }
+        else{
+            return ("User could not be deleted because it does not exist");
+        }
+    }
+
+    /**
+     * prints the list of all rooms in the house
+     * @return
+     */
+    public HashMap printRoomList(){
+        return House.roomList;
+
+    }
+
+    /**
+     * prints all appliances linked to a certain room
+     * @param roomName
+     * @return
+     */
+    public HashMap getRoomApplianceList(String roomName){
+        if(House.roomList.containsKey(roomName)){
+            return House.accessRoom(roomName).applianceMap;}
+        else{
+            return null;
+        }
+    }
+
+    /**
+     * prints all lights linked to a certain room
+     * @return
+     */
+    public HashMap getRoomLightList(String roomName){
+        return House.accessRoom(roomName).lightMap;
+
+    }
+
+}
