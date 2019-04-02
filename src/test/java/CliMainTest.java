56cfa4c1fa0e9bfbf50ca553f3d36ffad81d853b
apizzo <adpizzo96@gmail.com>
2019-04-02 11:42:33 -0400

    Added a CLI main class.

---
+++ src/test/java/CliMainTest.java
@@ -0,0 +1,9 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Scanner;
+import java.lang.String;
+
+public class CliMainTest {
+
+}
--- src/test/java/RoomTest.java
+++ src/test/java/RoomTest.java
@@ -9,7 +9,7 @@ public class RoomTest {
    @Test
     public void addApplianceTest(){
         Room room = new Room("testRoom");
-        room.addAppliance("TV", false,false);
+        room.addAppliance("TV", false);
         assertTrue(room.applianceMap.containsKey("TV"));
         assertFalse(room.applianceMap.containsKey("NotMyTV"));
 
@@ -19,7 +19,7 @@ public class RoomTest {
     @Test
     public void removeApplianceTest(){
         Room room = new Room("testRoom");
-        room.addAppliance("TV", false,false);
+        room.addAppliance("TV", false);
         assertTrue(room.applianceMap.containsKey("TV"));
         room.removeAppliance("TV");
         assertFalse(room.applianceMap.containsKey("TV"));
@@ -29,7 +29,7 @@ public class RoomTest {
     @Test
     public void changeApplianceStatusTest(){
         Room room = new Room("testRoom");
-        room.addAppliance("TV", false,false);
+        room.addAppliance("TV", false);
         room.changeApplianceStatus("TV");
         //TODO once you can get appliance status
 
