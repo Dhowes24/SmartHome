3d6ba3dba797b1a9b7b63fa707f32050740cf8e2
Derek Howes <D.howes24@gmail.com>
2019-05-01 20:15:29 -0400

    Leaving real quick to got to master

--- src/main/java/ScheduleItem.java
+++ src/main/java/ScheduleItem.java
@@ -1,11 +1,19 @@
 public class ScheduleItem {
     String Time;
     Boolean StatusTo;
+    Double value = null;
     Integer type;
 
     public ScheduleItem(String Time, Boolean StatusTo, Integer type){
         this.Time = Time;
         this.StatusTo = StatusTo;
+        this.type = type;
+    }
+
+    public ScheduleItem(String Time, Boolean StatusTo,Double value, Integer type){
+        this.Time = Time;
+        this.StatusTo = StatusTo;
+        this.value = value;
         this.type = type;
     }
 }
